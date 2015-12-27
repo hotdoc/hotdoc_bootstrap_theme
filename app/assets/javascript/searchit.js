de1d61e5841347ba07b0b99b4fd4dd9f53cc5d1e
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2015-12-27 22:12:11 +0100

    Fix sentence regex

--- app/assets/javascript/searchit.js
+++ app/assets/javascript/searchit.js
@@ -1,17 +1,18 @@
-var my_string = 'struct GBytes A simple refcounted data type representing an immutable sequence of zero or more bytes from an unspecified origin. The purpose of a GBytes is to keep the memory region that it holds alive for as long as anyone holds a reference to the bytes. When the last reference count is dropped, the memory is released. Multiple unrelated callers can use byte data in the GBytes without coordinating their activities, resting assured that the byte data will not change or move while they hold a reference. A GBytes can come from many different origins that may have different procedures for freeing the memory region. Examples are memory from g_malloc(), from memory slices, from a GMappedFile or memory from other allocators. GBytes work well as keys in GHashTable. Use g_bytes_equal() and g_bytes_hash() as parameters to g_hash_table_new() or g_hash_table_new_full(). GBytes can also be used as keys in a GTree by passing the g_bytes_compare() function to g_tree_new(). The data pointed to by this bytes must not be modified. For a mutable array of bytes see GByteArray. Use g_bytes_unref_to_array() to create a mutable array for a GBytes sequence. To create an immutable GBytes from a mutable GByteArray, use the g_byte_array_free_to_bytes() function. struct';
-
 function escapeRegExp(string) {
 	return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
 }
 
-function ellipsize (fragment, term, size_goal) {
+function ellipsize_fragment (fragment, term, size_goal) {
 	var words_remaining = (fragment.match(/\S+/g) || []).length;
 
+	console.log('ellipsizing', fragment);
+
 	if (words_remaining < size_goal) {
+		console.log('already compact enough');
 		return fragment;
 	}
 
-	var sentences = fragment.match(/[^\.!\?]+([\.!\?]+|$)/g);
+	var sentences = fragment.match(/[^\.!\?]+([\.!\?]+\s|$)/g);
 	var regex = new RegExp(escapeRegExp(term), "gi");
 	var nmatches = (fragment.match(regex) || []).length;
 	var matches_goal = Math.min(nmatches, size_goal / 20);
@@ -24,8 +25,10 @@ function ellipsize (fragment, term, size_goal) {
 	for (var i = 0; i < sentences.length; i++) {
 		var sentence = sentences[i];
 
-		if (backbuffer != undefined && backbuffer.length) {
+		if (backbuffer != undefined && backbuffer.length && passthrough == 0) {
+			console.log("But I did drop stuff ??");
 			result += '... ';
+			console.log(backbuffer, backbuffer.length);
 		}
 
 		backbuffer = [];
@@ -43,7 +46,8 @@ function ellipsize (fragment, term, size_goal) {
 				passthrough = words_per_match - start_index;
 
 				if (start_index > 0 && start_index < backbuffer.length) {
-					result += 'ELLIPSIS ';
+					console.log('something weird');
+					result += 'ELLIPSISAMIGO ';
 				}
 
 				for (var k = start_index; k < j; k++) {
@@ -65,6 +69,8 @@ function ellipsize (fragment, term, size_goal) {
 				/* Break awaaaaay !!! */
 				j = words.length;
 				i = sentences.length;
+				console.log("Breaking away");
+				result += '...';
 				break;
 			}
 
@@ -76,11 +82,7 @@ function ellipsize (fragment, term, size_goal) {
 		}
 	}
 
+	console.log('ellipsized', words_included);
+
 	return result;
 }
-
-console.time("compacter");
-for (var i=0; i < 10000; i++) {
-	ellipsize(my_string, 'bytes', 100);
-}
-console.timeEnd("compacter");
