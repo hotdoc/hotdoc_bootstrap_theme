27f3b5341bc2fb2f8738329531a631ce4a3c3ceb
Mathieu Duponchelle <mathieu@centricular.com>
2023-04-25 01:52:51 +0200

    styleswitcher: not all <a> elements have rel attributes

--- src/js/styleswitcher.js
+++ src/js/styleswitcher.js
@@ -1,6 +1,10 @@
 function setActiveStyleSheet(title) {
   var i, a, main;
   for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+    if (!a.hasAttribute('rel')) {
+      continue;
+    }
+
     if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title")) {
       a.disabled = true;
       if(a.getAttribute("title") == title) a.disabled = false;
@@ -20,6 +24,10 @@ function setActiveStyleSheet(title) {
 function getActiveStyleSheet() {
   var i, a;
   for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+    if (!a.hasAttribute('rel')) {
+      continue;
+    }
+
     if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title") && !a.disabled) return a.getAttribute("title");
   }
   return null;
@@ -28,6 +36,10 @@ function getActiveStyleSheet() {
 function getPreferredStyleSheet() {
   var i, a;
   for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+    if (!a.hasAttribute('rel')) {
+      continue;
+    }
+
     if(a.getAttribute("rel").indexOf("style") != -1
        && a.getAttribute("rel").indexOf("alt") == -1
        && a.getAttribute("title")
